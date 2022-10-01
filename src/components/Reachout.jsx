4fd1b4efabe03c348460efa03201815c877e2962
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2022-10-01 13:32:56 +0530

    added Modal for address

--- src/components/Reachout.jsx
+++ src/components/Reachout.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import "../styles/components/Reachout.css"
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
+import Popup from 'reactjs-popup';
 
 const Reachout = () => {
 
@@ -30,19 +31,32 @@ const Reachout = () => {
 
   return (
     <motion.div className="reachoutBody" ref={ref} animate={animation}>
+      <h1>Reach out to us!</h1>
 
-      <h1>
-        Reach out to us!
-      </h1>
-
-      <div className='reachoutFlexbox'>
+      <div className="reachoutFlexbox">
         <div className="reachoutButton" onClick={() => navigate("/about-us")}>
           <p>About Soul Medic</p>
         </div>
 
-        <div className="reachoutButton" onClick={() => window.open("mailto:sarjirijul@gmail.com")}>
-          <p>Contact Us</p>
-        </div>
+        <Popup
+          trigger={
+            <div
+              className="reachoutButton"
+              onClick={() => window.open("mailto:sarjirijul@gmail.com")}
+            >
+              <p>Contact Us</p>
+            </div>
+          }
+          modal
+        >
+          <div className="reachoutModal">
+            <h1>Phone Number</h1>
+            <p>+91 99998 98562</p>
+            <br />
+            <h1>Email ID</h1>
+            <a href='mailto:'>abcd@gmail.com</a>
+          </div>
+        </Popup>
       </div>
     </motion.div>
   );
